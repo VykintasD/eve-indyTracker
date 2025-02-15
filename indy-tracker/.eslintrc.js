f6eb9f3c0086568da17caf1eec91cca542191035
VykintasD <danc.vykintas@gmail.com>
2025-02-15 21:08:17 +0000

    feat: add concurrently, create npm workspaces

--- indy-tracker/.eslintrc.js
+++ indy-tracker/.eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-    parser: 'babel-eslint',  // Use Babel parser for JavaScript
-    extends: [
-      'plugin:vue/essential',  // For Vue support
-      'eslint:recommended',  // Basic ESLint rules
-    ],
-    env: {
-      browser: true,  // Allow browser globals
-      node: true,     // Allow Node.js globals
-    },
-    rules: {
-      // Your custom ESLint rules here
-    },
-  };
-  
+  parserOptions: {
+    ecmaVersion: 2021, // Ensure modern JS support
+    sourceType: "module",
+  },
+  extends: [
+    "plugin:vue/vue3-recommended", // Use Vue 3 specific rules
+    "eslint:recommended", // Basic ESLint rules
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    "vue/multi-word-component-names": "off", // Disable multi-word component name rule
+  },
+};
